e20c3203a02cbda7fafff1b777639dd1d729ee0a
Lulu Fu <lulumsp7@gmail.com>
2026-02-06 18:27:56 -0500

    update part b

--- src/main/java/FileHandler.java
+++ src/main/java/FileHandler.java
@@ -1,24 +1,23 @@
 import java.io.File;
 import java.util.ArrayList;
 public class FileHandler{
-    //private variable, file list
-    private ArraryList<String> fileList;
-    //constructor
-    public FileHandler(){
-        File folder = new File("data");
+    public ArrayList<String> getFileList(){
+        ArrayList<String> fileList=new ArrayList<>();
+        if (!folder.exists() || !folder.isDirectory()) {
+            throw new IllegalStateException("Data folder not found.");
+        }
         File[] data = folder.listFiles();
-        String str="";
-        for(int i=0;i<data.length;i++){
-            if(data[i].isFile()){
-                str=data[i].getName()+".txt";
+        if(data.length==0){
+            throw new IllegalStateException("Data folder is empty.");
+        }
+        String str = "";
+        for (int i = 0; i < data.length; i++) {
+            if (data[i].isFile()) {
+                str = data[i].getName();
                 fileList.add(str);
             }
         }
-    }
-
-    public ArrayList<String> getFileList(){
+        Collections.sort(fileList);
         return fileList;
     }
-
-
 }
