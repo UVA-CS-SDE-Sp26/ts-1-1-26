291c360a2ccc068fa366268c773cdc424043d075
Lulu Fu <lulumsp7@gmail.com>
2026-02-06 17:19:22 -0500

    Implement FileHandler for Part B

--- src/main/java/FileHandler.java
+++ src/main/java/FileHandler.java
@@ -2,31 +2,51 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class FileHandler{
-    File[] fileList;
+    //private variable, file list
+    private File[] fileList;
+    //constructor
     public FileHandler(){
-        File folder = new File(".data");
+        File folder = new File("data");
         if (folder.exists() && folder.isDirectory()) {
-            File[] fileList = folder.listFiles();
+            fileList = folder.listFiles();
+        }
+        else{
+            fileList=new File[0];
         }
     }
-    public String readFileList() throws FileNotFoundException{
-        if(fileList != null){
-            for(File file: fileList){
-                System.out.println(file.getName());
-            }
-        }
-}   public String readFile(int index){
+    //getter for file list
+    public File[] getFileList() {
+        return fileList;
+    }
+//    //read the file list
+//    public String readFileList() throws FileNotFoundException{
+//        String str="";
+//        if(fileList != null){
+//            for(File file: fileList){
+//                str=fileList+file.getName();
+//            }
+//        }
+//        return str;
+//}
+    //read the exact file indexed
+    public String readFile(int index){
         if(index>=fileList.length){
             throw new IndexOutOfBoundsException("file not in the list");
         }
+            File currentFile=fileList[index];
+            String str=this.readFile(currentFile);
+
+        return str;
     }
+    //helper method to read files
     public String readFile(File file) throws FileNotFoundException {
         Scanner myReader = new Scanner(file);
         String str="";
         while (myReader.hasNextLine()) {
             String data = myReader.nextLine();
-            str=str+data;
+            str=str+data+"\n";
         }
+        myReader.close();
         return str;
     }
 
