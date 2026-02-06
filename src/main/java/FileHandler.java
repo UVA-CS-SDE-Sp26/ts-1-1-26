65e51ca604ff6dd063dc34584d18917907b55058
Lulu Fu <lulumsp7@gmail.com>
2026-02-06 16:47:33 -0500

    Part B: file handler implementation

--- src/main/java/FileHandler.java
+++ src/main/java/FileHandler.java
@@ -2,27 +2,32 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class FileHandler{
-    public static void main(String[] args) {
+    File[] fileList;
+    public FileHandler(){
         File folder = new File(".data");
         if (folder.exists() && folder.isDirectory()) {
             File[] fileList = folder.listFiles();
-
-
-
-    public String readFile() throws FileNotFoundException{
-        if (fileList != null) {
-            for (File file : fileList) {
+        }
+    }
+    public String readFileList() throws FileNotFoundException{
+        if(fileList != null){
+            for(File file: fileList){
                 System.out.println(file.getName());
             }
         }
-    }}
+}   public String readFile(int index){
+        if(index>=fileList.length){
+            throw new IndexOutOfBoundsException("file not in the list");
+        }
+    }
     public String readFile(File file) throws FileNotFoundException {
-
         Scanner myReader = new Scanner(file);
+        String str="";
         while (myReader.hasNextLine()) {
             String data = myReader.nextLine();
-            System.out.println(data);
+            str=str+data;
         }
+        return str;
     }
 
 }
