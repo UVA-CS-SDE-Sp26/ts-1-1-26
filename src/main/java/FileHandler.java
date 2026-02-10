2d2c0d8ae578c3eac0347b91b58b7aa63e705018
Lulu Fu <lulumsp7@gmail.com>
2026-02-10 01:09:10 -0500

    update on Part B

--- src/main/java/FileHandler.java
+++ src/main/java/FileHandler.java
@@ -1,7 +1,9 @@
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
 public class FileHandler{
     public ArrayList<String> getFileList(){
+        File folder=new File("data");
         ArrayList<String> fileList=new ArrayList<>();
         if (!folder.exists() || !folder.isDirectory()) {
             throw new IllegalStateException("Data folder not found.");
--- src/test/java/FileHandlerTest.java
+++ src/test/java/FileHandlerTest.java
@@ -1,2 +1,19 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
 public class FileHandlerTest {
+    @Test
+    public void testFolderNotFound(){}
+
+    @Test
+    public void testGetFlileList_Success(){
+        FileHandler fh=new FileHandler();
+        ArrayList<String> fileList=fh.getFileList();
+        assertNotNull(fileList);
+        assertEquals("filea.txt",fileList.get(0));
+        assertEquals("fileb.txt",fileList.get(1));
+        assertEquals("filec.txt",fileList.get(2));
+    }
+
 }
