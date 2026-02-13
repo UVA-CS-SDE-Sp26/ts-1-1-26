0a691cf3b67aa296577ea05d9d4b08c0f2df116f
Daniel <haozhangcong@gmail.com>
2026-02-13 00:23:58 -0500

    final edits

--- src/test/java/MockTestC.java
+++ src/test/java/MockTestC.java
@@ -1,6 +1,6 @@
 //this is a mock test, delete this later.
 //fast compile in terminal: javac src\main\java\*.java src\test\java\MockTestC.java
-//fast run in terminal: java -cp "src\main\java" MockTestC a
+//fast run in terminal: java -cp "src\main\java" MockTestC
 public class MockTestC {
 
     partC test = new partC();
